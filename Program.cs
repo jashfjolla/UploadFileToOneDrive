6053176159d2ecd546d6e596be77609dc0cf0cb7
Fjolla Jashari <jashfjolla@gmail.com>
2026-09-06 13:29:28 +0200

    Adding code structure and handling file arguments

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿if (args.Length == 0)
+{
+    Console.Error.WriteLine("Please provide a file path.");
+    return 1;
+}
+
+string filePath = args[0];
+
+if (!File.Exists(filePath))
+{
+    Console.Error.WriteLine($"File has not been found: {filePath}");
+    return 1;
+}
+
+var file = new FileInfo(filePath);
+
+Console.WriteLine($"This file has been found {file.FullName} with this size {file.Length} bytes");
+
+Console.WriteLine("Ready to upload.");
+
+return 0;
